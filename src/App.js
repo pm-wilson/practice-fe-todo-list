7e3808ad13a9f889979a255ac6532a9b7c47878d
Patrick Wilson <pmwilson333@gmail.com>
2020-08-19 12:03:56 -0700

    initial commit dev with router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import ListPage from './ListPage.js';
+import HomePage from './HomePage.js';
 
-function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <HomePage {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/ListPage" 
+                            exact
+                            render={(routerProps) => <ListPage {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function HomePage() {
+  
+  return (
+    <div>
+      Home Page
+    </div>
+  );
+}
+
+export default HomePage;
